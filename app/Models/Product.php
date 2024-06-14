61d9ceb662f6faf170aba3d1ab272cc5fdd1ee29
MykhailoGrygorenko <osnova2021@gmail.com>
2024-06-14 19:13:21 +0300

    added images table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title', 'description', 'price', 'created_at',
+    ];
+
+    protected $hidden = [
+        'updated_at'
+    ];
+
+    public function Images(): HasMany
+    {
+        return $this->hasMany('images');
+    }
 }
--- database/migrations/2024_06_14_160254_create_images_table.php
+++ database/migrations/2024_06_14_160254_create_images_table.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('description');
-            $table->decimal('price');
+            $table->unsignedBigInteger('product_id');
+            $table->string('path');
             $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('images');
     }
 };
