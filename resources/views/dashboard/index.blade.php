7d89fe2d79d239d9a8ac9de523193584178e0896
MykhailoGrygorenko <osnova2021@gmail.com>
2024-06-15 21:59:55 +0300

    finished index styling

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-comm</title>
+    <title>Lite E-comm Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <header>
-        <div class="wrapper">
-            <h1>Lite e-comm dashboard</h1>
+    <header class>
+        <div class="btn-group group">
+            <h1>Lite E-comm Dashboard</h1>
         </div>
     </header>
     
@@ -22,28 +23,52 @@
             </aside>
 
             <main>
-            @foreach ($products as $product)
-                <div class="card">
-                    <h2>{{ $product->title }}</h2>
-                    <p>{{ $product->description }}</p>
-                    <p>Price: ${{ $product->price }}</p>
-                    @foreach ($product->images as $image)
-                        <img src="{{ asset('storage/' . $image->path) }}" alt="Product Image">
-                    @endforeach
-                    <button type="button" class="delete-button" data-product-id="{{$product->id}}">Delete</button>
-                    <div class="server-error" style="display: none;">An error occured while attempting to delete this product</div>
-                </div>
-            @endforeach
+                @foreach ($products as $product)
+                    <div class="card p-4 mb-4">
+                        @foreach ($product->images as $image)
+                            <img src="{{ asset('storage/' . $image->path) }}" alt="Product Image" class="card-img-top">
+                        @endforeach
+                        <div class="about-product p-1 card-body">
+                            <h5 class="card-title">{{ $product->title }}</h5>
+                            <p class="card-text">Price: ${{ $product->price }}</p>
+                        </div>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detailsModal{{ $product->id }}">
+                            Details
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="detailsModal{{ $product->id }}" tabindex="-1" aria-labelledby="detailsModal{{ $product->id }}Label" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="detailsModal{{ $product->id }}Label">{{ $product->title }} description</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>{{ $product->description }}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </main>
         </div>
     </div>
 
     <footer>
-
     </footer>
-    <input type="hidden" id="token" value="{{csrf_token()}}">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    @vite('resources/css/dashboard.css')
+    
+    <input type="hidden" id="token" style="display:none;" value="{{ csrf_token() }}">
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     @vite('resources/js/dashboard.js')
+    @vite('resources/css/dashboard.css')
 </body>
 </html>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,45 +4,80 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-comm</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-
-    <header>
-        <button></button>
+    <header class="p-4 container-xl">
+        <div class="btn-group group">
+            <p>Some E-comm</p>
+            <button class="btn btn-primary rounded" style="max-width: min-content;">
+                Catalog
+            </button>
+        </div>
+        <input type="text" name="" id="" class="p-1" style="width:50%;">
+        <nav class="group">
+            <a href="">Purchases</a>
+            <a href="">Profile</a>
+        </nav>
     </header>
-    <div class="container-lg">
+    <div class="container-xl" style="padding: 0;">
+        <div class="container">
             <aside>
-                <ul>
-                    <li><a href="">Category1</a></li>
-                    <li><a href="">Category2</a></li>
-                    <li><a href="">Category3</a></li>
-                    <li><a href="">Category4</a></li>
-                    <li><a href="">Category5</a></li>
-                    <li><a href="">Category6</a></li>
-                    <li><a href="">Category7</a></li>
-                    <li><a href="">Category8</a></li>
-                    <li><a href="">Category9</a></li>
-                    <li><a href="">Category10</a></li>
+                <ul class="list-group">
+                    <li class="list-group-item"><a href="">Category1</a></li>
+                    <li class="list-group-item"><a href="">Category2</a></li>
+                    <li class="list-group-item"><a href="">Category3</a></li>
+                    <li class="list-group-item"><a href="">Category4</a></li>
+                    <li class="list-group-item"><a href="">Category5</a></li>
+                    <li class="list-group-item"><a href="">Category6</a></li>
+                    <li class="list-group-item"><a href="">Category7</a></li>
+                    <li class="list-group-item"><a href="">Category8</a></li>
+                    <li class="list-group-item"><a href="">Category9</a></li>
+                    <li class="list-group-item"><a href="">Category10</a></li>
                 </ul>
             </aside>
             <main>
                 @foreach ($products as $product)
-                    <div class="card">
-                        <h2>{{ $product->title }}</h2>
-                        <p>{{ $product->description }}</p>
-                        <p>Price: ${{ $product->price }}</p>
+                    <div class="card p-4" style="position: relative;">
                         @foreach ($product->images as $image)
-                            <img src="{{ asset('storage/' . $image->path) }}" alt="Product Image">
+                            <img src="{{ asset('storage/' . $image->path) }}" alt="Product Image" style="card-img-top">
                         @endforeach
+                        <div class="about-product p-1 card-body" style="min-height: 170px">
+                            <h5 class="card-title">{{ $product->title }}</h5>
+                            <p class="card-text">Price: ${{ $product->price }}</p>
+                        </div>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target={{"#detailsModal".$product->id}} style="max-width: min-content;">
+                        Details
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id={{"detailsModal".$product->id}} tabindex="-1" aria-labelledby="{{"detailsModal".$product->id}}Label" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="{{"detailsModal".$product->id}}Label">{{$product->title}} description</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p>{{ $product->description }}</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
                     </div>
                 @endforeach
             </main>
         </div>
+    </div>
     <footer>
     </footer>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     @vite('resources/css/app.css')
 </body>
 </html>
