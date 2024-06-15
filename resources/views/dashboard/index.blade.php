78719390e08af397d560fc3fb7d9825ccce25ca9
MykhailoGrygorenko <osnova2021@gmail.com>
2024-06-15 22:10:58 +0300

    finished dashboard styling

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lite E-comm Dashboard</title>
+    <title>E-comm</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <header class>
-        <div class="btn-group group">
-            <h1>Lite E-comm Dashboard</h1>
+    <header>
+        <div class="container-xl p-4">
+            <h1>Lite e-comm dashboard</h1>
         </div>
     </header>
     
-    <div class="container">
+    <div class="container-xl p-4">
         <div class="wrapper">
             <aside>
                 <ul>
@@ -24,35 +24,38 @@
 
             <main>
                 @foreach ($products as $product)
-                    <div class="card p-4 mb-4">
+                    <div class="card p-4" style="position: relative;">
                         @foreach ($product->images as $image)
-                            <img src="{{ asset('storage/' . $image->path) }}" alt="Product Image" class="card-img-top">
+                            <img src="{{ asset('storage/' . $image->path) }}" alt="Product Image" style="card-img-top">
                         @endforeach
-                        <div class="about-product p-1 card-body">
+                        <div class="about-product p-1 card-body" style="min-height: 170px">
                             <h5 class="card-title">{{ $product->title }}</h5>
                             <p class="card-text">Price: ${{ $product->price }}</p>
                         </div>
                         <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detailsModal{{ $product->id }}">
-                            Details
-                        </button>
+                        <div>
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target={{"#detailsModal".$product->id}} style="max-width: min-content;">Details</button>
+                            <button type="button" class="btn btn-primary btn-danger delete-button" data-product-id={{$product->id}} style="max-width: min-content;">Delete</button>
+                        </div>
+                        
+                        
 
                         <!-- Modal -->
-                        <div class="modal fade" id="detailsModal{{ $product->id }}" tabindex="-1" aria-labelledby="detailsModal{{ $product->id }}Label" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="detailsModal{{ $product->id }}Label">{{ $product->title }} description</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <p>{{ $product->description }}</p>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    </div>
-                                </div>
+                        <div class="modal fade" id={{"detailsModal".$product->id}} tabindex="-1" aria-labelledby="{{"detailsModal".$product->id}}Label" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="{{"detailsModal".$product->id}}Label">{{$product->title}} description</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
+                            <div class="modal-body">
+                                <p>{{ $product->description }}</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                            </div>
+                        </div>
                         </div>
                     </div>
                 @endforeach
@@ -61,14 +64,13 @@
     </div>
 
     <footer>
+
     </footer>
-    
-    <input type="hidden" id="token" style="display:none;" value="{{ csrf_token() }}">
-    
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <input type="hidden" id="token" style="display:none;" value="{{csrf_token()}}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-    @vite('resources/js/dashboard.js')
     @vite('resources/css/dashboard.css')
+    @vite('resources/js/dashboard.js')
 </body>
 </html>
